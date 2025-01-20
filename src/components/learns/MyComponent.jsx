9c203033c0a163ad96586ac636285912c9eba43f
Duynguyen <duynguyen0444@gmail.com>
2025-01-20 22:47:34 +0700

    JSX with parameters

--- src/components/learns/MyComponent.jsx
+++ src/components/learns/MyComponent.jsx
@@ -2,9 +2,21 @@ import { Fragment } from "react";
 import "./styles.css";
 
 const MyComponent = () => {
+  const name = "MyComponent";
+  const undefinedVariable = undefined;
+  const arrays = [1, 2, 3, 4, 5];
+  const objects = { name: "MyComponent", age: 30 };
+
   return (
     <Fragment>
-      <div className="child">MyComponent</div>
+      <div className="child">
+        {name} - {undefinedVariable}
+      </div>
+
+      {/* Convert the following arrays and objects to String: */}
+      <div>{JSON.stringify(objects)}</div>
+      <div>{JSON.stringify(arrays)}</div>
+
       <div style={{ borderRadius: 20 }}>MyComponent 1</div>
     </Fragment>
   );
