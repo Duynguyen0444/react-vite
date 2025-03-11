4d17a8f9d762a836e373dc79b4a56656a14b4198
Duynguyen <duynguyen0444@gmail.com>
2025-03-11 22:44:06 +0700

    organze structure

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LoginPage from "./pages/login.jsx";
+import RegisterPage from "./pages/register.jsx";
+import UserPage from "./pages/user.jsx";
+import ProductPage from "./pages/product.jsx";
+import "./styles/global.css";
 
 const router = createBrowserRouter([
   {
@@ -10,19 +15,19 @@ const router = createBrowserRouter([
   },
   {
     path: "/login",
-    element: <div>Login</div>,
+    element: <LoginPage />,
   },
   {
     path: "/register",
-    element: <div>Register</div>,
+    element: <RegisterPage />,
   },
   {
     path: "/user",
-    element: <div>User</div>,
+    element: <UserPage />,
   },
   {
     path: "/product",
-    element: <div>Product</div>,
+    element: <ProductPage />,
   },
 ]);
 
